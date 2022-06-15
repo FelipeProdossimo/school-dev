34ac2a11e51956e17256ba10bab5d7e2e25943c7
Felipe Prodóssimo <73621753+FelipeProdossimo@users.noreply.github.com>
2022-06-15 11:47:14 -0300

    Melhorando código e incluindo funções.
    Obter, editar e excluir.

--- SmartSchool/src/app/alunos/alunos.component.ts
+++ SmartSchool/src/app/alunos/alunos.component.ts
@@ -1,36 +1,50 @@
 import { Aluno } from './../models/Aluno';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-alunos',
   templateUrl: './alunos.component.html',
-  styleUrls: ['./alunos.component.css']
+  styleUrls: ['./alunos.component.css'],
 })
 export class AlunosComponent implements OnInit {
-
+  public alunoForm: FormGroup;
   public titulo = 'Alunos';
   public alunoSelecionado: Aluno;
   public textSimple: string;
 
   public alunos = [
-   { id: 1,nome: 'Julia', sobrenome: 'Louin', telefone: 22334455} ,
-   { id: 2,nome: 'Camila', sobrenome: 'Notato', telefone: 22334125} ,
-   { id: 3,nome: 'Rogerio', sobrenome: 'Monopapo', telefone: 223344535} ,
-   { id: 4,nome: 'Cauã', sobrenome: 'Rato', telefone: 2254746455} ,
-   { id: 5,nome: 'Lindomar', sobrenome: 'Cajamar', telefone: 22334512}
+    { id: 1, nome: 'Julia', sobrenome: 'Louin', telefone: 22334455 },
+    { id: 2, nome: 'Camila', sobrenome: 'Notato', telefone: 22334125 },
+    { id: 3, nome: 'Rogerio', sobrenome: 'Monopapo', telefone: 223344535 },
+    { id: 4, nome: 'Cauã', sobrenome: 'Rato', telefone: 2254746455 },
+    { id: 5, nome: 'Lindomar', sobrenome: 'Cajamar', telefone: 22334512 },
   ];
+
+  constructor(private fb: FormBuilder) {
+    this.criarForm();
+  }
+
+  ngOnInit(): void {}
+
+  criarForm() {
+    this.alunoForm = this.fb.group({
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      telefone: ['', Validators.required],
+    });
+  }
+
+  alunoSubmit() {
+    console.log(this.alunoForm.value);
+  }
 
   alunoSelected(aluno: Aluno) {
     this.alunoSelecionado = aluno;
+    this.alunoForm.patchValue(aluno);
   }
 
-voltar() {
-  this.alunoSelecionado = null;
-}
-
-  constructor() { }
-
-  ngOnInit(): void {
+  voltar() {
+    this.alunoSelecionado = null;
   }
-
 }
--- SmartSchool/src/app/professores/professores.component.ts
+++ SmartSchool/src/app/professores/professores.component.ts
@@ -1,35 +1,48 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Professor } from './../models/Professor';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-professores',
   templateUrl: './professores.component.html',
-  styleUrls: ['./professores.component.css']
+  styleUrls: ['./professores.component.css'],
 })
 export class ProfessoresComponent implements OnInit {
-
+  public profForm: FormGroup;
   public titulo = 'Professores';
   public profSelecionado: Professor;
 
   public professores = [
-    { id: 1 ,nome: 'Ricadin', disciplina: 'Matemática'},
-    { id: 2 ,nome: 'Rovania', disciplina: 'Física'},
-    { id: 3 ,nome: 'Islovenia', disciplina: 'Português'},
-    { id: 4 ,nome: 'Kindin', disciplina: 'Inglês'},
-    { id: 5 ,nome: 'Kuririn', disciplina: 'Programação'}
-  ]
+    { id: 1, nome: 'Ricadin', disciplina: 'Matemática' },
+    { id: 2, nome: 'Rovania', disciplina: 'Física' },
+    { id: 3, nome: 'Islovenia', disciplina: 'Português' },
+    { id: 4, nome: 'Kindin', disciplina: 'Inglês' },
+    { id: 5, nome: 'Kuririn', disciplina: 'Programação' },
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.criarForm();
+  }
+
+  ngOnInit() {}
+
+  criarForm() {
+    this.profForm = this.fb.group({
+      nome: ['', Validators.required],
+      disciplina: ['', Validators.required],
+    });
+  }
+
+  profSubmit() {
+    console.log(this.profForm.value);
+  }
 
   profSelected(professor: Professor) {
     this.profSelecionado = professor;
+    this.profForm.patchValue(professor);
   }
 
-voltar() {
-  this.profSelecionado = null;
-}
-
-  constructor() { }
-
-  ngOnInit() {
+  voltar() {
+    this.profSelecionado = null;
   }
-
 }

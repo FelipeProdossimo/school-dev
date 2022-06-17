f14a12e5587567aae2169968dcb350198644d42f
Felipe Prodóssimo <73621753+FelipeProdossimo@users.noreply.github.com>
2022-06-17 16:54:16 -0300

    Banco de dados - OK e ASP.NET 10/10
    Realizando Backend com Visual Studio Code, puxando informações com ASP.NET Core WebAPI + Banco de Dados fluindo perfeitamente.

---
+++ SmartSchool-WebAPI/Controllers/AlunoController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool_WebAPI.Controllers{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunoController : ControllerBase {
+
+        [HttpGet]
+        public IActionResult Get(){
+            return BadRequest("Felipe");
+        }
+    }
+}
